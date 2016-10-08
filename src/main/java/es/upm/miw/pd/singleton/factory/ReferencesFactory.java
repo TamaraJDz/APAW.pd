d96856ada85252398294add0fff652d5b6465c10
Tamara <tamara_uky@hotmail.com>
2016-10-08 22:42:27 +0200

    singleton.factory finalized

---
+++ src/main/java/es/upm/miw/pd/singleton/factory/ReferencesFactory.java
@@ -0,0 +1,34 @@
+package es.upm.miw.pd.singleton.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReferencesFactory {
+	
+	private static ReferencesFactory referenceSing = new ReferencesFactory();;
+    private Map<String, Integer> references;
+    private int reference;
+    
+    private ReferencesFactory() {
+        this.references = new HashMap<>();
+        this.reference = 0;
+    }
+
+    public static ReferencesFactory getFactory() {
+        return ReferencesFactory.referenceSing;
+    }
+    
+    public int getReference(String key) {
+        Integer result = this.references.get(key);
+        if (result == null) {
+            this.references.put(key, this.reference);
+            result = this.reference;
+            reference++;
+        }
+        return result;
+    }
+
+    public void removeReference(String key) {
+        this.references.remove(key);
+    }
+}
